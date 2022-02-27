abc1164d51c93100d754a864a620ddbff8051635
vemareddy119 <41448745+vemareddy119@users.noreply.github.com>
2022-02-27 19:38:35 +0530

    arrayEx

---
+++ src/com/arrays/ArraysExample.java
@@ -0,0 +1,14 @@
+package com.arrays;
+
+public class ArraysExample {
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/vemareddy119/LeetCodeBootcamp.git
+//    git push -u origin main
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4};
+        System.out.println("First Element of the array is: "+arr[0]);
+    }
+}
